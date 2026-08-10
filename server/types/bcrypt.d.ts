9e0aaa71853e71727f3fcade7fc8909ed87c0daf
alex <alex@mail.ru>
2026-08-10 12:29:42 +0300

    upd

--- server/types/bcrypt.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'bcrypt' {
-  const bcrypt: {
-    compare(data: string, encrypted: string): Promise<boolean>
-    hash(data: string, saltOrRounds: string | number): Promise<string>
-  }
-
-  export default bcrypt
-}
